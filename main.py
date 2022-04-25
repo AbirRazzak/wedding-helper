50c29a35896fb68b438cbb666d936e53bc42cae0
Abir Razzak <Abirrazzak1@gmail.com>
2022-04-25 10:16:56 -0400

    Created a scripts directory

--- main.py
+++ main.py
@@ -16,12 +16,4 @@ def setup_environment() -> environs.Env:
 if __name__ == '__main__':
     env = setup_environment()
     save_the_date_parser = SaveTheDateResponsesParser.new(env)
-    # pprint(sorted(save_the_date_parser.get_names_of_responders()))
-
-    everyone_on_notion = get_names_of_everyone_on_the_list(env.path('NOTION_CSV_FILE_PATH'))
-
-    filter_set = set(save_the_date_parser.get_names_of_responders())
-
-    c = [x for x in everyone_on_notion if x not in filter_set]
-
-    pprint(sorted(c))
+    pprint(sorted(save_the_date_parser.get_names_of_responders()))
---
+++ scripts/determine_who_hasnt_responded_to_save_the_date.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+from main import setup_environment
+from master_list.csv_parser import get_names_of_everyone_on_the_list
+from save_the_date.csv_parser import SaveTheDateResponsesParser
+
+if __name__ == '__main__':
+    env = setup_environment()
+    save_the_date_parser = SaveTheDateResponsesParser.new(env)
+    print('The following people have responded to the save the date:')
+    pprint(sorted(save_the_date_parser.get_names_of_responders()))
+
+    everyone_on_notion = get_names_of_everyone_on_the_list(env.path('NOTION_CSV_FILE_PATH'))
+
+    filter_set = set(save_the_date_parser.get_names_of_responders())
+
+    people_who_have_not_responded = [x for x in everyone_on_notion if x not in filter_set]
+
+    print('The following people have not responded to the save the date:')
+    pprint(sorted(people_who_have_not_responded))
