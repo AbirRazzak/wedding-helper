7006c2c5667286c128c9908217a87842a79cdbae
Abir Razzak <Abirrazzak1@gmail.com>
2022-05-20 18:35:09 -0400

    Parse SaveTheDateResponse's and convert into WithjoyGuestList

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import environs
 
+from save_the_date.repo import SaveTheDateResponseRepo
+from save_the_date.response_parser import SaveTheDateResponseParser
+from withjoy.converter import WithjoyConverter
+
 
 def setup_environment() -> environs.Env:
     _env = environs.Env()
@@ -11,3 +15,11 @@ def setup_environment() -> environs.Env:
 if __name__ == '__main__':
     env = setup_environment()
     print('Hello World!')
+    save_the_date_response_repo = SaveTheDateResponseRepo.new(
+        env=env,
+        parser=SaveTheDateResponseParser()
+    )
+    withjoy_guest_list = WithjoyConverter().convert_save_the_date_repo_into_withjoy_guest_list(
+        save_the_date_repo=save_the_date_response_repo
+    )
+    print(withjoy_guest_list)
